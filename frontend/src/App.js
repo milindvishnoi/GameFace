7dee01f24e83e3db9311949c93368c1cfdc9f0fd
mills <milindvishnoi@gmail.com>
2021-02-19 17:33:53 +0530

    Added Routes and matrial ui theme setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path='/' render={ () => <Home />} />
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default App;
---
+++ frontend/src/theme.js
@@ -0,0 +1,9 @@
+import { createMuiTheme }  from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+  palette: {
+    
+  },
+})
+
+export default theme
