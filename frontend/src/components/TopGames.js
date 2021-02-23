8f21f4ca60e51a60c07a48e8b1b824e5b365d212
mills <milindvishnoi@gmail.com>
2021-02-23 15:50:56 +0530

    Checkpoint 2 topGames

--- frontend/src/components/TopGames.js
+++ frontend/src/components/TopGames.js
@@ -1,29 +1,24 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { ButtonBase, Grid, Typography } from '@material-ui/core'
-import './topGame.css'
 
 export default class TopGames extends Component {
   static propTypes = {
-    name: Text,
-    imgSrc: Text
+    name: PropTypes.string,
+    imgSrc: PropTypes.string
   }
 
   render() {
+    const {name, imgSrc} = this.props
     return (
       <Grid item xs={12} sm={6}>
         <ButtonBase
           focusRipple
-          key={this.props.name}
-          className="image"
+          key={name}
         >
-          <image 
-            src={this.props.imgSrc}
-            className="imageScr"
-            />
+          <img src={process.env.PUBLIC_URL + imgSrc} />
+          <Typography variant="h5">{name}</Typography>
         </ButtonBase>
-        
-        <Typography variant="h5">{this.props.name}</Typography>
       </Grid>
     )
   }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,31 +1,20 @@
-import { Box, Typography } from '@material-ui/core'
+import { Box, Grid, Typography } from '@material-ui/core'
 import React, { Component } from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import TopGames from '../components/TopGames';
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+// import nba from '../public/topGames/nba2k22.jpg'
 
 const topGames = [
   {
     name: "NBA 2K22",
-    imgSrc: "../../public/topGames/nba2k22.jpg"
+    imgSrc: "/topGames/nba2k22.jpg",
+  },
+  {
+    name: "NBA 2K22",
+    imgSrc: "/topGames/nba2k22.jpg",
+  },
+  {
+    name: "NBA 2K22",
+    imgSrc: "/topGames/nba2k22.jpg",
   }
 ]
 
@@ -38,11 +27,14 @@ export class Home extends Component {
         </Box>
 
         <Typography variant='h3'>Top 10 Games</Typography>
-        {topGames.map((row) => (
-          <TopGames imgSrc={row.imgSrc}
-                    name={row.name}
-                    key={row.name} />
-        ))}
+        <Grid container spacing={4}>
+          {topGames.map((row) => (
+            <TopGames imgSrc={row.imgSrc}
+                      name={row.name}
+                      key={row.name} />
+          ))}
+        </Grid>
+        {/* <img src={nba} /> */}
       </div>
     )
   }
