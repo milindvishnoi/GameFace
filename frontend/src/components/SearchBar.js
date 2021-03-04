33f5a95fcf31bd17cb4a741768ee717309abb910
mills <milindvishnoi@gmail.com>
2021-03-05 02:52:13 +0530

    Checkpoint 2 for searchbar

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-import { TextField, Typography } from '@material-ui/core'
+import { TextField, List, Box, ListItem, ListItemText } from '@material-ui/core'
 import AutoComplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
-// import { Link } from 'react-router-dom'
 import './searchBar.css'
 import { Link } from 'react-router-dom';
 
@@ -13,47 +12,64 @@ const filterOptions = createFilterOptions({
 export class SearchBar extends Component {
   state = {
       games: [
-        { name: 'NBA 2K22', 
+        { 
+          name: 'NBA 2K22', 
           imgSrc: '/topGames/nba2k22.jpg',
           link: 'nba2k22'
         },
-        { name: 'Fifa 2021', 
+        { 
+          name: 'Fifa 2021', 
           imgSrc: '/topGames/nba2k22.jpg',
           link: 'fifa2021'
         },
-        { name: 'Underdog', 
+        { 
+          name: 'Underdog', 
           imgSrc: '/topGames/nba2k22.jpg',
           link: 'ud'
         },
-        { name: 'Asphault', 
+        { 
+          name: 'Asphault', 
           imgSrc: '/topGames/nba2k22.jpg',
           link: 'asphault'
         },
       ],
       searchField : '',
-      searchedGame: []
+      searchedGame: [],
+      displaySearch: false,
   }
 
-  // handleSearch = (event) => {
-  //   const value = event.target.value
-  //   console.log(value)
+  handleSearch = (event) => {
+    const value = event.target.value
+    console.log(value)
 
-  //   const searchedGames = value !== '' ? 
-  //   this.state.games.filter(game => 
-  //     game.name.toLowerCase().includes(value.toLowerCase())
-  //   ) : []
-  //   console.log(searchedGames)
+    const searchedGames = value !== '' ? 
+    this.state.games.filter(game => 
+      game.name.toLowerCase().includes(value.toLowerCase())
+    ) : []
+    console.log(searchedGames)
 
-  //   this.setState({
-  //     searchedGame: searchedGames,
-  //     searchField: value
-  //   })
-  // }
+    this.setState({
+      searchedGame: searchedGames,
+      searchField: value
+    })
+  }
+
+  hideSuggestions = () => {
+    this.setState({
+      displaySearch: true
+    })
+  }
+
+  showSuggestions = () => {
+    this.setState({
+      displaySearch: false
+    })
+  }
 
   render() {
     return (
-      <div>
-          <AutoComplete
+      <div className='relative-pos'>
+          {/* <AutoComplete
               className='bar'
               id='search-bar'
               clearOnBlur
@@ -67,11 +83,38 @@ export class SearchBar extends Component {
                   variant="outlined"
                 />
               )}
+              loading
               getOptionLabel={(game) => game.name}
-            />
+              renderOption={(game) => (<div className='link'><Box w={100}>{game.name}</Box></div>)}
+            /> */}
+
+            {/* <input type='text'
+                   placeholder='Search Games...'
+                   onKeyUp={this.handleSearch}
+                   onBlur={hideSuggestions}
+                   onFocus={showSuggestions}
+                   />
+            
+            <ul className='search-suggestions'>
+              {(!this.state.displaySearch && this.state.searchedGame.map((game, index) => {
+                return (
+                  <li key={index} className={focusIndex === index ? 'active-search' : null}>
+                    <Link href={`/${game.link}`} underline='none' color='inherit' variant='body1'>
+                      {game.name}
+                    </Link>
+                  </li>
+                )
+                }))}
+            </ul> */}
+
+          <input type='text'
+                 className='bar'
+                 placeholder='Search Games...'
+                 onKeyUp={this.handleSearch}
+                />
         
-        {/* {this.state.searchedGame.length !== 0 ? 
-        <template className='search-results-container'>
+        {this.state.searchedGame.length !== 0 ? 
+        <List className='search-results-container absolute'>
           {this.state.searchedGame.map(game => {
             return (
               <ListItem button to={game.link} key={game.name}>
@@ -81,8 +124,8 @@ export class SearchBar extends Component {
               </ListItem>
             )
             })}
-        </template> : null
-        } */}
+        </List> : null
+        }
       </div>
     )
   }
