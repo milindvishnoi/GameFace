c5d2b7cd112ffb7d8195ba1419ec5a2d075ef86d
anoverga17 <gabino6821@gmail.com>
2021-03-08 00:21:28 -0500

    Completed Basic Game Page with Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Container, CssBaseline, Switch as Swap } from '@material-ui/core'
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Game from './pages/Game'
 import LoginPage from './pages/LoginPage'
 import SignUpPage from './pages/SignUpPage'
 import PersonalPage from './pages/PersonalPage'
@@ -9,6 +10,7 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import { darkTheme, lightTheme } from './theme'
 import Navbar from './components/global/header/Navbar';
 import Footer from './components/global/footer/Footer';
+
 
 export class App extends Component {
   state = {
@@ -31,6 +33,7 @@ export class App extends Component {
           <Navbar theme={darkMode} toggleTheme={this.toggleTheme} />
             <Container>
               <Switch>
+                <Route exact path='/games/nba2k22' render={ () => <Game />} />
                 <Route exact path='/' render={ () => <Home />} />
                 <Route exact path='/login' render={ () => <LoginPage />} />
                 <Route exact path='/signup' render={ () => <SignUpPage />} />
---
+++ frontend/src/pages/Game.js
@@ -0,0 +1,92 @@
+import { Box, Button, DialogTitle, Grid, Typography } from '@material-ui/core'
+import React, { Component } from 'react'
+import './GamePage.css'
+
+// The appropriate game data to be imported from a server (description is from the PS4 website for 2K21)
+const gameData = [
+  {
+    title: "NBA2K22",
+    score: "77",
+    description: "NBA 2K22 is the latest title in the world-renowned, \
+                  best-selling NBA 2K series, delivering an industry-leading \
+                  sports video game experience on console.\n\n \
+                  With extensive improvements upon its best-in-class \
+                  graphics and gameplay, competitive and community \
+                  online features, and deep, varied game modes, NBA \
+                  2K22 offers one-of-a-kind immersion into all facets \
+                  of NBA basketball and culture - where Everything is Game.",
+    tags: ["Sports", "2K", "Single Player", "Multiplayer", "PS4", "XBox"]
+  }
+]
+
+//The game selected to display
+const displayGame = gameData[0];
+
+export class Game extends Component {
+    render() {
+      const splitDescription = (str) => {
+        {/* Splits <str> appropriatley depending on where \n is in the text */}
+        return(
+          <div>
+            {str.split("\n").map((pargph) => (<Typography>{pargph}<br/></Typography>))}
+          </div>
+        )
+      }
+      return (
+        <div>
+          <GameHeader gameTitle={displayGame.title}
+                      rating={displayGame.score}
+                      description={ () => splitDescription(displayGame.description)}/> 
+          <Box textAlign="right">
+            <Button>Create New Review</Button>
+          </Box>
+        </div>
+      )
+    }
+  }
+
+  class GameHeader extends Component {
+    render() {
+      const { gameTitle, description, rating } = this.props;
+
+      return (
+          <Box marginTop="2vh">
+            <Grid container spacing={5}>
+              <Grid item>
+                <img class="gameIcon" src="/topGames/nba2k22.jpg" />
+                <div align="center">
+                  <Typography>Rating: { rating }%</Typography>
+                  <button>Upvote</button>
+                </div>
+              </Grid>
+              <Grid id="descriptionPanel" item>
+                <Typography variant='h2'>{ gameTitle }</Typography>
+                <Typography><br/>{ description() }<br/></Typography>
+                {/* For Game Admin and above only:
+                  <div style={{marginTop: 2}}>
+                    <Button>Create Tag</Button>
+                  </div>
+                */}
+                <Grid container spacing={2}>
+                  {displayGame.tags.map((tagContent) => (<Tag content={tagContent}/>))}
+                </Grid>
+              </Grid>
+            </Grid>
+          </Box>
+          
+      )
+    }
+  }
+
+  class Tag extends Component {
+    render () {
+      const { content } = this.props;
+      return (
+        <Grid id="tag" item>
+          <Typography>{ content }</Typography>
+        </Grid>
+      )
+    }
+  }
+
+  export default Game
