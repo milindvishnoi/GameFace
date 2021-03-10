b042f448101628e22a7371bbf78be79956e7c36b
putongzhongdan <51966424+putongzhongdan@users.noreply.github.com>
2021-03-10 08:28:20 -0500

    Add files via upload
    Added profile tab bar.

---
+++ frontend/src/pages/ProfileTab/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import { AppBar, Tabs, Tab } from '@material-ui/core'
+import ProfileTabPanel from './../ProfileTabPanel'
+import DiscussionPanel from './../DiscussionPanel'
+import GamePanel from "./../GamePanel"
+
+
+export class ProfileTab extends Component {
+  state = {
+      page: 0
+  }
+  
+  handleChangeTab = (event, val) => {
+    this.setState({
+        page: val
+    })
+
+  }
+
+  render() {
+    
+
+    return (
+      
+      <div> 
+        <AppBar position='static'>
+            <Tabs
+                onChange={this.handleChangeTab}
+                value={this.state.page} 
+                aria-label="profile tabs">
+              <Tab label="Profile" />
+              <Tab label="Discussions" />
+              <Tab label="Games" />
+
+            </Tabs>
+        </AppBar>
+        <ProfileTabPanel 
+          index={0}
+          page={this.state.page}
+        />
+
+        <DiscussionPanel 
+          index={1}
+          page={this.state.page}
+        />
+        
+        <GamePanel
+          index={2}
+          page={this.state.page}
+        />
+
+      </div>   
+    )
+  }
+}
+
+export default ProfileTab
