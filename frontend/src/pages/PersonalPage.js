cce642af0ef12905d54a7e8846496c87192d64dc
putongzhongdan <51966424+putongzhongdan@users.noreply.github.com>
2021-03-10 08:12:01 -0500

    Add files via upload
    Modified  personal profile page.

--- frontend/src/pages/PersonalPage.js
+++ frontend/src/pages/PersonalPage.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
-import { Box, Typography, Button, Container, AppBar, Toolbar } from '@material-ui/core'
+import { Box, Typography, Button, Container, AppBar, Toolbar, Tabs, Tab } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 import { user } from '../data'
 import './PersonalPage.css';
+import ProfileTab from './ProfileTab';
 
 // Inspired by (URL)
 export class PersonalPage extends Component {
   state={
     email: '',
     emailError: false,
-    emailErrorMessage: ''
+    emailErrorMessage: '',
+    page: 1
   }
 
   validate = (event) => {
@@ -51,36 +53,8 @@ export class PersonalPage extends Component {
               </Typography>
             </Box>
 
-          <AppBar position='static'>
-            <Container>
-              <Toolbar>
-                <Box>
-                  <Typography className="personal-navbar"
-                    variant="h4" 
-                    noWrap 
-                  >
-                    Profile
-                  </Typography>
-                </Box>
-                <Box>
-                  <Typography className="personal-navbar"
-                    variant="h4" 
-                    noWrap 
-                  >
-                    Discussion 
-                  </Typography>
-                </Box>
-                <Box>
-                  <Typography className="personal-navbar"
-                    variant="h4" 
-                    noWrap 
-                  >
-                    Games
-                  </Typography>
-                </Box>
-              </Toolbar>
-            </Container>
-          </AppBar>
+          <ProfileTab />
+          
           <Link to='/'>
             <Button className="logout-button" onClick={ logout }>Logout</Button>
           </Link>
