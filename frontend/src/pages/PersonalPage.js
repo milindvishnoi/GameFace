a42f72e30d13c7b6ce1f9f6cb2a07cd32c6a7188
mills <milindvishnoi@gmail.com>
2021-03-11 06:29:14 +0530

    fixed style for personal page

--- frontend/src/pages/PersonalPage.js
+++ frontend/src/pages/PersonalPage.js
@@ -7,41 +7,17 @@ import ProfileTab from './ProfileTab';
 
 // Inspired by (URL)
 export class PersonalPage extends Component {
-  state={
-    email: '',
-    emailError: false,
-    emailErrorMessage: '',
-    page: 1
-  }
-
-  validate = (event) => {
-    const target = event.target
-    const value = target.value
-    const name = target.name
-    let reg = new RegExp(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/).test(value)
-    console.log(reg)
-    reg ? this.setState({
-        [name]: value,
-        emailErrorMessage: "",
-        error: false
-      }) : this.setState({
-        [name]: value,
-        emailErrorMessage: "Please fill in a valid email.",
-        error: true
-      })
-  }
-
   render() {
     const { logout } = this.props
 
     return (
-        <Box className='personal-profile'>
+        <Box>
             <Box>
                 <img 
                   className='cover-image'
                   src={ process.env.PUBLIC_URL + user.coverPic } />
             </Box>
-            <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center'>
+            <Box mb={5} display='flex' flexDirection='column' justifyContent='center' alignItems='center'>
               <img 
                 className='personal-picture'
                 src={ process.env.PUBLIC_URL + user.profilePic } />
@@ -51,13 +27,14 @@ export class PersonalPage extends Component {
               <Typography className="userBio" variant="h5">
                 { user.bio }
               </Typography>
+              <Box mt={2}>
+                <Link to='/'>
+                  <Button className="logout-button" onClick={ logout }>Logout</Button>
+                </Link>
+              </Box>
             </Box>
 
           <ProfileTab />
-          
-          <Link to='/'>
-            <Button className="logout-button" onClick={ logout }>Logout</Button>
-          </Link>
       </Box>
     )
   }
