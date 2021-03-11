c5404e5afe92a00659c0ada0ee8536d0119ef86a
putongzhongdan <51966424+putongzhongdan@users.noreply.github.com>
2021-03-10 19:51:43 -0500

    Add files via upload

--- frontend/src/pages/PersonalPage.js
+++ frontend/src/pages/PersonalPage.js
@@ -8,50 +8,56 @@ import ProfileTab from './ProfileTab';
 // Inspired by (URL)
 export class PersonalPage extends Component {
   state={
+    email: '',
+    emailError: false,
+    emailErrorMessage: '',
     page: 1
+  }
+
+  validate = (event) => {
+    const target = event.target
+    const value = target.value
+    const name = target.name
+    let reg = new RegExp(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/).test(value)
+    console.log(reg)
+    reg ? this.setState({
+        [name]: value,
+        emailErrorMessage: "",
+        error: false
+      }) : this.setState({
+        [name]: value,
+        emailErrorMessage: "Please fill in a valid email.",
+        error: true
+      })
   }
 
   render() {
     const { logout } = this.props
-    
+
     return (
-        <Box>
+        <Box className='personal-profile'>
             <Box>
                 <img 
                   className='cover-image'
                   src={ process.env.PUBLIC_URL + user.coverPic } />
             </Box>
-            <Box 
-              my={3} 
-              display='flex' 
-              flexDirection='column' 
-              justifyContent='center' 
-              alignItems='center'
-              >
+            <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center'>
               <img 
                 className='personal-picture'
                 src={ process.env.PUBLIC_URL + user.profilePic } />
-              <Typography variant="h4">
+              <Typography className="userName" variant="h3">
                 @{ user.username }
               </Typography>
-              <Typography className="userBio" variant="body1">
+              <Typography className="userBio" variant="h5">
                 { user.bio }
               </Typography>
-              <Box mb={2} />
             </Box>
 
           <ProfileTab />
-          <Box
-            my={3}
-            display='flex' 
-            flexDirection='column' 
-            justifyContent='center' 
-            alignItems='center'
-          >
-            <Link to='/'>
-              <Button variant='outlined' className="logout-button" onClick={ logout }>Logout</Button>
-            </Link>
-          </Box>
+          
+          <Link to='/'>
+            <Button className="logout-button" onClick={ logout }>Logout</Button>
+          </Link>
       </Box>
     )
   }
