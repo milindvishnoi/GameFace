c8050dd8f99022796e40daecb300ab5c7306f14a
putongzhongdan <51966424+putongzhongdan@users.noreply.github.com>
2021-03-10 20:05:24 -0500

    Add files via upload

--- frontend/src/pages/PersonalPage.js
+++ frontend/src/pages/PersonalPage.js
@@ -4,38 +4,82 @@ import { Link } from 'react-router-dom'
 import { user } from '../data'
 import './PersonalPage.css';
 import ProfileTab from './ProfileTab';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import ImportContactsIcon from '@material-ui/icons/ImportContacts';
 
 // Inspired by (URL)
 export class PersonalPage extends Component {
-  render() {
-    const { logout } = this.props
+  state={
+    page: 1
+  }
 
-    return (
+  render() {
+    const { logout, userLoggedIn, gameAdminLoggedIn, siteAdminLoggedIn } = this.props;
+    const isLoggedIn = userLoggedIn || gameAdminLoggedIn || siteAdminLoggedIn;
+    const isAdmin = gameAdminLoggedIn || siteAdminLoggedIn;
+    const showProfile = () => {if (isLoggedIn) {
+      return (
         <Box>
             <Box>
                 <img 
                   className='cover-image'
                   src={ process.env.PUBLIC_URL + user.coverPic } />
             </Box>
-            <Box mb={5} display='flex' flexDirection='column' justifyContent='center' alignItems='center'>
+            <Box 
+              my={3} 
+              display='flex' 
+              flexDirection='column' 
+              justifyContent='center' 
+              alignItems='center'
+              >
               <img 
                 className='personal-picture'
                 src={ process.env.PUBLIC_URL + user.profilePic } />
-              <Typography className="userName" variant="h3">
+              <Typography variant="h4">
                 @{ user.username }
+                {(isAdmin) ? <SupervisorAccountIcon /> : ""}
               </Typography>
-              <Typography className="userBio" variant="h5">
+              <Typography className="userBio" variant="body1">
                 { user.bio }
               </Typography>
-              <Box mt={2}>
-                <Link to='/'>
-                  <Button className="logout-button" onClick={ logout }>Logout</Button>
-                </Link>
-              </Box>
+              <Box mb={2} />
             </Box>
 
+          {isAdmin ? manageButton() : ""}
+          
           <ProfileTab />
+          <Box
+            my={3}
+            display='flex' 
+            flexDirection='column' 
+            justifyContent='center' 
+            alignItems='center'
+          >
+            <Link to='/'>
+              <Button variant='outlined' className="logout-button" onClick={ logout }>Logout</Button>
+            </Link>
+          </Box>
       </Box>
+      )
+    }}
+
+    const manageButton = () => {
+      return (
+      <Link to='/' className="game__manage-button">
+        <Button
+        variant="outlined"
+        startIcon={<ImportContactsIcon />}
+      >
+        Manage Game
+      </Button>
+      </Link>
+      )
+    }
+
+    return (
+      <div>
+        {showProfile()}
+      </div> 
     )
   }
 }
