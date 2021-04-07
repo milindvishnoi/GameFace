25b0dc01dcddf44f34c23d459f53a661579faaee
anoverga17 <gabino6821@gmail.com>
2021-04-07 10:46:41 -0400

    Added login integration

---
+++ client/src/actions/users.js
@@ -0,0 +1,48 @@
+import ENV from '../config.js'
+const API_HOST = ENV.api_host
+const log = console.log
+
+// A function to send a POST request with the user to be logged in
+export const login = (loginComp, app) => {
+    // Create our request constructor with all the parameters we need
+    const request = new Request(`${API_HOST}/users/login`, {
+        method: "post",
+        body: JSON.stringify(loginComp.state),
+        headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+        }
+    });
+
+    // Send the request with fetch()
+    fetch(request)
+        .then(res => {
+            if (res.status === 200) {
+                return res.json();
+            }
+        })
+        .then(json => {
+            if (json.currentUser !== undefined) {
+                app.setState({ currentUser: json.currentUser });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
+
+// A function to send a GET request to logout the current user
+export const logout = (app) => {
+    const url = `${API_HOST}/users/logout`;
+
+    fetch(url)
+        .then(res => {
+            app.setState({
+                currentUser: null,
+                message: { type: "", body: "" }
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
